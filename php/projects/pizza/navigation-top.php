1b5dd6b3507442edee8de2a206f9bfbcde56fb08
aczietlow <aczietlow@spyderbytedesign.com>
2012-06-26 14:51:15 -0400

    Reorganized Directory

---
+++ php/projects/pizza/navigation-top.php
@@ -0,0 +1,58 @@
+<!--  
+<div id="mainnav"><a href="index.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;
+	<a class="menu" href="#" onClick="javascript: window.open('mainmenu.php','radio_player', 'Width=795,Height=1196,directories=0,hotkeys=0,location=0,menubar=0,personalbar=0,resizable=0,scrollbars=1,status=0,toolbar=0');">Menu</a>
+&nbsp;&nbsp;&nbsp;&nbsp;T-Shirts&nbsp;&nbsp;&nbsp;&nbsp;
+Events&nbsp;&nbsp;&nbsp;&nbsp; FUNraising&nbsp;&nbsp;&nbsp;&nbsp; 
+	<a href="augusta-attractions.php">Area Info</a>&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="teletray.php">Teletray Project</a> 
+	&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="../find-us.php">Find Us</a>
+	</div>
+-->
+<ul id="navlist">
+	<li> 
+		<a href="index.php" style="width: 60px;">Home</a>
+	</li>
+	<li>
+		<a class="menu" href="#" onClick="javascript: window.open('mainmenu.php','radio_player', 'Width=795,Height=1196,directories=0,hotkeys=0,location=0,menubar=0,personalbar=0,resizable=0,scrollbars=1,status=0,toolbar=0');">Menu</a>
+	</li>
+	<li>
+		<a href="index.php" 
+		onmouseover="mopen('n1')" 
+        onmouseout="mclosetime()" style="width: 60px;">Events</a>
+		<div id="n1" 
+			onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+			<a href="bike-night.php">Bike-Night</a>
+			<a href="cruise-in.php">Cruise-In</a>
+			<a href="movie-night.php">Movie-Night</a>
+			<a href="index.php">FUNraising</a>
+		</div>
+	</li>
+	<li>
+		<a href="bike-night.php" style="width: 90px;">Bike-Night</a>
+	</li>
+	<li>
+		<a href="cruise-in.php">Cruise-In</a>
+	</li>
+	<li>
+		<a href="t-shirts.php" style="width: 70px;">T-Shirts</a>
+	</li>
+	<li>
+		<a href="augusta-attractions.php">Area Info</a>
+	</li>
+	<li>
+		<a href="index.php" 
+		onmouseover="mopen('n2')" 
+        onmouseout="mclosetime()" style="width: 80px;">About Us</a>
+		<div id="n2" 
+			onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+			<a href="history.php">History</a>
+			<a href="teletray.php">Teletray Project</a>
+		</div>
+	</li>
+	<li>
+		<a href="../find-us.php" style="width: 70px;">Find Us</a>
+	</li>
+</ul>
