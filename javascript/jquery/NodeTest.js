8eacfb38790ff64cad9c974da794baf5f394e1b1
aczietlow <aczietlow@spyderbytedesign.com>
2013-02-05 21:17:19 -0500

    played with some jquery

---
+++ javascript/jquery/NodeTest.js
@@ -0,0 +1,11 @@
+var http =  require('http');
+var url =  require('url');
+
+http.createServer(function (req,res) {
+	console.log('Request:' + req.method + " to " + req.url);
+	
+	res.writeHead(200, "OK");
+	res.write("<h1>Hello</h1>Node.js is working");
+	res.end();
+}).listen(80);
+console.log("ready on port 80");
